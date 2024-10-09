bc4304a30e17f0c3724d8f74bb736fb0b3f53a54
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-10-09 09:12:40 +0800

    October 6 2024 2024 - Phone Navigation and removing hrefs

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
     <!-- Logo
     ================================================== -->
     <div class="span5 logo">
-        <a href="index.htm"><img src="{{ asset('img/Logo.png') }}" style="height: 150px; width: 150px"alt="" /></a>
+        <a href=""><img src="{{ asset('img/Logo.png') }}" style="height: 150px; width: 150px"alt="" /></a>
         <h5>Museo ni Miguel Malvar </h5>
     </div>
 
@@ -44,29 +44,11 @@
             <div class="mobile-nav-select">
                 <select onchange="window.open(this.options[this.selectedIndex].value,'_top')">
                     <option value="">Navigate...</option>
-                    <option value="index.htm">Home</option>
-                    <option value="index.htm">- Full Page</option>
-                    <option value="index-gallery.htm">- Gallery Only</option>
-                    <option value="index-slider.htm">- Slider Only</option>
-                    <option value="features.htm">Features</option>
-                    <option value="page-full-width.htm">Pages</option>
-                    <option value="page-full-width.htm">- Full Width</option>
-                    <option value="page-right-sidebar.htm">- Right Sidebar</option>
-                    <option value="page-left-sidebar.htm">- Left Sidebar</option>
-                    <option value="page-double-sidebar.htm">- Double Sidebar</option>
-                    <option value="gallery-4col.htm">Gallery</option>
-                    <option value="gallery-3col.htm">- 3 Column</option>
-                    <option value="gallery-4col.htm">- 4 Column</option>
-                    <option value="gallery-6col.htm">- 6 Column</option>
-                    <option value="gallery-4col-circle.htm">- Gallery 4 Col Round</option>
-                    <option value="gallery-single.htm">- Gallery Single</option>
-                    <option value="blog-style1.htm">Blog</option>
-                    <option value="blog-style1.htm">- Blog Style 1</option>
-                    <option value="blog-style2.htm">- Blog Style 2</option>
-                    <option value="blog-style3.htm">- Blog Style 3</option>
-                    <option value="blog-style4.htm">- Blog Style 4</option>
-                    <option value="blog-single.htm">- Blog Single</option>
-                    <option value="page-contact.htm">Contact</option>
+                    <option value="{{ url('/') }}">Home</option>
+                    <option value="{{ url('gallery') }}">- Gallery</option>
+                    <option value="{{ route('event') }}">- Events</option>
+                    <option value="{{ url('contact') }}">- Contact</option>
+                    <option value="{{ route('login') }}">Login</option>
                 </select>
             </div>
         </form>
